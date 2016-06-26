f72df0b5a721354ebac84516d9313566cabc1dc7
Sagar <coolsagarpopat@gmail.com>
2016-06-26 14:38:46 +0600

    Update captcha.php

--- Captcha-decode/captcha.php
+++ Captcha-decode/captcha.php
@@ -1,49 +1,74 @@
-<!DOCTYPE html>
-
-<?php 
+<?php
 
 session_start();
 
-if(!isset($_SESSION['result']))
+if(!isset($_SESSION['time']))
+{
+
+    $t = time();    //timestamp
+    $_SESSION['otime'] = $t;
+    $final = $t + 120;
+    $_SESSION['time'] = $final; 
+    create_image();
+    
+}
+
+else
 {
     
-    $_SESSION['result'] = 0;
+    if(time() > $_SESSION['time'])
+    {
+
+    
+        session_destroy();
+        
+    }
+    else
+    {
+        create_image();
+
+    }   
 }
+
+
+function create_image()
+{
+
+    header("Content-Type: image/png");
+    $image = imagecreatetruecolor(200, 50) or die("Cannot Initialize new GD image stream");
+
+    $background_color = imagecolorallocate($image, 0, 0, 0);
+    $text_color = imagecolorallocate($image, 122, 111, 255);
+    $line_color = imagecolorallocate($image, 64, 64, 64);
+    $pixel_color = imagecolorallocate($image, 0, 0, 255);
+
+    imagefilledrectangle($image, 0, 0, 200, 50, $background_color);
+
+    for ($i = 0; $i < 3; $i++) {
+        imageline($image, 0, rand(0 ,255) % 50, 200, rand(0, 255) % 50, $line_color);
+    }
+
+    for ($i = 0; $i < 1000; $i++) {
+        imagesetpixel($image, rand() % 200, rand() % 50, $pixel_color);
+    }
+
+
+    $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $len = strlen($letters);
+    $letter = $letters[rand(0, $len - 1)];
+   
+   # $text_color = imagecolorallocate($image, 0, 0, 0);
+    $word = "";
+    for ($i = 0; $i < 6; $i++) {
+        $letter = $letters[rand(0, $len - 1)];
+        imagestring($image, 7, 5 + ($i * 30), 20, $letter, $text_color);
+        $word .= $letter;
+    }
+    $_SESSION['solution'] = $word;
+    imagepng($image);
+    imagedestroy($image);
+
+}
+
+
 ?>
-<html>
-    <head>
-        <script>
-           function error()
-            {
-
-               alert("Session Expired");
-               location.reload(); 
-           }
-         </script>
-        <title>Captcha challenge</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-    <center><div>Break captcha for 99 times in 120 Seconds and get the flag: </div>
-    <form action="verify.php" method="post"> 
-            <img src="imagedemo.php" onerror=javascript:error();><br>
-            Enter Code:<input type="text" name="solution" /> 
-            <input type="submit" name="Submit" value="Submit" /> <br><br>
-
-            <p>Note: Your session will be expired in 120 Seconds. </p> <br>
-            
-           <?php
-           
-           echo 'Score :'.$_SESSION['result'];
-           
-           if($_SESSION['result'] > 99)
-           {  
-               echo "Flag is H@CKIM_C@pTcha!09022015";
-           }
-           ?> 
-</form>
-
-    </center>
-    </body>
-</html>
